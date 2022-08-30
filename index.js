3c7fc3a8a20b7c20e32588ec6b811527bec40c97
sathninduk <sathnidukottage@gmail.com>
2022-08-30 23:01:09 +0530

    core

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-exports.printMsg = function() {
-    return "This is a message from the demo package";
+const jwt = require('jsonwebtoken');
+const ethers = require('ethers');
+
+exports.generate = function (address, secret) {
+    function generateRandomToken(length) {
+        let result = '';
+        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() *
+                charactersLength));
+        }
+        return result;
+    }
+
+    let randomToken = generateRandomToken(32);
+    return jwt.sign({name: "ether-auth token", address: address, randomToken: randomToken}, secret, "", "");
+}
+
+exports.validate = function (token, signature, secret) {
+    let decodedJwt = jwt.verify(token, secret, "", "");
+    try {
+        const signerAddr = ethers.utils.verifyMessage(token, signature);
+        if (signerAddr === decodedJwt.address) {
+            return {address: decodedJwt.address};
+        } else {
+            return false;
+        }
+    } catch (err) {
+        return false;
+    }
 }
